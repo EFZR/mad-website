df27bfcf4836abd3558313c2917bb3cc0f29d9b3
efzr <emerson.zapatarivas@gmail.com>
2024-05-12 23:51:15 -0600

    fix: responsive

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Hero from "@components/Hero";
 import About from "@components/About";
 import Members from "@components/Members";
 import Form from "@components/Form";
+import Footer from "@components/Footer";
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
       <About />
       <Members />
       <Form />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import "@styles/Footer.css";
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <div className="container grid footer__container">
+        Footer
+      </div>
+    </footer>
+  )
+}
